5c6bc337250089c87e71609fc073161a1b59123c
Johsua Gore (Desktop) <Joshua.Gore8@gmail.com>
2019-02-23 20:26:15 +1030

    updates to map (flies to hallet cove)

--- cesium-webpack-app/src/index.js
+++ cesium-webpack-app/src/index.js
@@ -9,17 +9,25 @@ var east = 138.5054;
 var north = -35.0667;
 
 // a two dimenstional region specified as longitude and latitude coordinates, in degrees
-var rectangle = Cesium.Rectangle.fromDegrees(west, south, east, north);
-
-// var heading = Cesium.HeadingPitchRange(1.5708, 0.785398);
+var parkExtent = Cesium.Rectangle.fromDegrees(west, south, east, north);
 
 // scale camera larger or smaller than DEFAULT_VIEW_RECTANGLE
-Cesium.Camera.DEFAULT_VIEW_FACTOR = 0;
+// Cesium.Camera.DEFAULT_VIEW_FACTOR = 0;
 
 // default rectangle camera views on creation
-Cesium.Camera.DEFAULT_VIEW_RECTANGLE = rectangle;
+// Cesium.Camera.DEFAULT_VIEW_RECTANGLE = rectangle;
 
-// default heading
-// Cesium.Camera.DEFAULT_OFFSET = heading;
+var viewer = new Cesium.Viewer('cesiumContainer', {
+    timeline: false, animation: false, homeButton: false, geocoder: false, creditContainer: "credits", creditViewport: "credits"
+});
 
-var viewer = new Cesium.Viewer('cesiumContainer', {timeline: false, animation: false, creditContainer: "credits", creditViewport: "credits" });
+
+var camera = viewer.camera;
+camera.flyTo({
+    // destination: Cesium.Cartesian3.fromDegrees(138.499209, -35.073201, 1500),
+    destination: Cesium.Cartesian3.fromDegrees(138.485, -35.073201, 1000),
+    orientation: {
+        heading: Cesium.Math.toRadians(90),
+        pitch: -Cesium.Math.toRadians(45),
+    }
+});
